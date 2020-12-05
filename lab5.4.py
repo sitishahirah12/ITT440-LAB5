7b79012560a3194339abf9d17adff6627a65abe7
sitishahirah12 <sitishahirah122@gmail.com>
2020-12-05 08:00:25 +0000

    Python for Client

---
+++ lab5.4.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+import json
+
+s= socket.socket()
+
+port = 8080
+
+s.connect(('192.168.56.110', port))
+
+data =s.recv(1024)
+data = data.decode("utf-8")
+
+s.send(b'Thank you for connecting!');
+
+dataJ = json.loads(data)
+
+
+print(type(dataJ))
+print(dataJ)
+
+s.close()
